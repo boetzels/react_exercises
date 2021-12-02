de98787b4aa7598b2a0e06cabfc66b08efa39476
sboetzel <71010527+boetzels@users.noreply.github.com>
2021-12-02 22:21:05 +0100

    add components folder and finish accordion ex

---
+++ search_filter/src/components/Accordion.js
@@ -0,0 +1,33 @@
+import '../App.css';
+import Chevron from './Chevron';
+import { useState, useRef } from "react";
+
+function Accordion(props) {
+  const [active, setActive] = useState(false);
+  const [contentHeight, setContentHeight] = useState("0px");
+
+  const accordionContent = useRef(null);
+
+  const toggleAccordion = () => {
+    setActive(!active);
+    setContentHeight((!active ? accordionContent.current.scrollHeight : 0)+'px');
+  };
+  
+  return (
+    <div key={ props.i } className="App AccordionSection">
+        <header className={ active ? "active" : "" } onClick={ () => toggleAccordion() }>
+          { props.title }
+          <Chevron
+            className={ active ? "chevron rotate" : "chevron" }
+            width={ 20 }
+            fill={ "#eee" }
+          />
+        </header>
+        <span ref={ accordionContent } style={{maxHeight:`${contentHeight}`}}>
+          <p>{ props.content }</p>
+        </span>
+    </div>
+  );
+}
+
+export default Accordion;
--- search_filter/src/index.js
+++ search_filter/src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SearchFilter from './SearchFilter';
+import Accordion from './components/Accordion';
 import reportWebVitals from './reportWebVitals';
+
+function App(props) {
+  const titles = [
+    'Lorem ipsum dolor sit amet',
+    'Consectetur adipiscing elit',
+  ];
+
+  const contents = [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris augue neque gravida in fermentum. A diam maecenas sed enim. Turpis egestas integer eget aliquet nibh praesent. Ipsum suspendisse ultrices gravida dictum. Orci a scelerisque purus semper eget duis at. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Massa id neque aliquam vestibulum morbi blandit. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Justo eget magna fermentum iaculis eu. Lorem ipsum dolor sit amet.',
+    'Metus dictum at tempor commodo ullamcorper a lacus vestibulum. Sit amet mauris commodo quis imperdiet massa. Ut lectus arcu bibendum at varius vel pharetra vel. Faucibus ornare suspendisse sed nisi. Ornare lectus sit amet est. Eget mi proin sed libero enim sed.',
+  ];
+
+  const nbr = titles.length;
+  const list = [];
+
+  for (let x = 0; x < nbr; x++) {
+    list.push(
+      <Accordion i={ x } title={ titles[x] } content={ contents[x] } />
+    )
+  }
+  
+  return(list);
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <SearchFilter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
