ca399845df2954364e00f169cf53f50f7b27a151
sboetzel <71010527+boetzels@users.noreply.github.com>
2021-12-02 22:35:29 +0100

    first commit imageslider

---
+++ search_filter/src/components/ImageSlider.js
@@ -0,0 +1,33 @@
+import '../ImageSlider.css';
+import Chevron from './Chevron';
+import { useState, useRef } from "react";
+
+function ImageSlider(props) {
+  const [active, setActive] = useState(false);
+  const [contentHeight, setContentHeight] = useState("0px");
+
+  const accordionContent = useRef(null);
+
+  const toggleAccordion = () => {
+    setActive(!active);
+    setContentHeight((!active ? accordionContent.current.scrollHeight : 0)+'px');
+  };
+  
+  return (
+    <div key={ props.i } className="App AccordionSection">
+        <header className={ active ? "active" : "" } onClick={ () => toggleAccordion() }>
+          { props.title }
+          <Chevron
+            className={ active ? "chevron rotate" : "chevron" }
+            width={ 20 }
+            fill={ "#eee" }
+          />
+        </header>
+        <span ref={ accordionContent } style={{maxHeight:`${contentHeight}`}}>
+          <p>{ props.content }</p>
+        </span>
+    </div>
+  );
+}
+
+export default ImageSlider;
